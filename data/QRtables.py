d19069754240387228ce89ddd683fadb04419780
anmol <bhandari@umn.edu>
2016-08-23 08:44:41 -0500

    cleaned up Khani's code

--- data/QRtables.py
+++ data/QRtables.py
@@ -4,44 +4,49 @@ Created on Mon Aug 22 12:30:13 2016
 Tabel 2 and 3 of QR
 @author: Mohammad
 """
+
+import pandas as pd
+import numpy as np
+import stats
+pd.options.display.float_format = '${:,.0f}'.format
+
 scf = pd.read_csv('scf.csv')
 
 ################## Table 2
 i =0
 Q = [0, .01, .05, .1, .2, .4, .5, .6, .8, .9, .95, .99, 1]
-qi , qw = np.zeros(len(Q)), np.zeros(len(Q))
-for q in Q:
-    qi[i]= quant(scf.INCOME, scf.WGT, q)
-    qw[i]= quant(scf.NETWORTH, scf.WGT, q)
-    i +=1
+qi=[stats.quantile_1D(scf.INCOME,scf.WGT,q) for q in Q]
+qw=[stats.quantile_1D(scf.NETWORTH,scf.WGT,q) for q in Q]
 data_for_plotting=pd.DataFrame(np.vstack((qi,qw)),columns=Q, index=['income', 'wealth'])
 print('\n'+'TABLE 2:')
 print(data_for_plotting)
 
+pd.options.display.float_format = '{:,.2f}'.format
+
 ################## Table 3
 #Income (INCOME2 can also be used!)
 si= np.zeros(8)
-si[0]=variation(scf.INCOME, scf.WGT)
-si[1]=variance(np.log(scf.INCOME[(scf.INCOME>0)]),scf.WGT[(scf.INCOME>0)])
-si[2]=gini(scf.INCOME, scf.WGT)
+si[0]=stats.coeff_variation(scf.INCOME, scf.WGT)
+si[1]=stats.variance(np.log(scf.INCOME[(scf.INCOME>0)]),scf.WGT[(scf.INCOME>0)])
+si[2]=stats.gini(scf.INCOME, scf.WGT)
 
-si[3]=loc(scf.INCOME, scf.WGT, mean(scf.INCOME, scf.WGT))
-si[4]=quant(scf.INCOME, scf.WGT, .99)/quant(scf.INCOME, scf.WGT, .5)
-si[5]=quant(scf.INCOME, scf.WGT, .9)/quant(scf.INCOME, scf.WGT, .5)
-si[6]=mean(scf.INCOME, scf.WGT)/quant(scf.INCOME, scf.WGT, .5)
-si[7]=quant(scf.INCOME, scf.WGT, .5)/quant(scf.INCOME, scf.WGT, .3)
+si[3]=stats.loc(scf.INCOME, scf.WGT, stats.mean(scf.INCOME, scf.WGT))
+si[4]=stats.quantile_1D(scf.INCOME, scf.WGT, .99)/stats.quantile_1D(scf.INCOME, scf.WGT, .5)
+si[5]=stats.quantile_1D(scf.INCOME, scf.WGT, .9)/stats.quantile_1D(scf.INCOME, scf.WGT, .5)
+si[6]=stats.mean(scf.INCOME, scf.WGT)/stats.quantile_1D(scf.INCOME, scf.WGT, .5)
+si[7]=stats.quantile_1D(scf.INCOME, scf.WGT, .5)/stats.quantile_1D(scf.INCOME, scf.WGT, .3)
 
 # Networth
 sw= np.zeros(8)
-sw[0]=variation(scf.NETWORTH, scf.WGT)
-sw[1]=variance(np.log(scf.NETWORTH[(scf.NETWORTH>0)]),scf.WGT[(scf.NETWORTH>0)])
-sw[2]=gini(scf.NETWORTH, scf.WGT)
+sw[0]=stats.coeff_variation(scf.NETWORTH, scf.WGT)
+sw[1]=stats.variance(np.log(scf.NETWORTH[(scf.NETWORTH>0)]),scf.WGT[(scf.NETWORTH>0)])
+sw[2]=stats.gini(scf.NETWORTH, scf.WGT)
 
-sw[3]=loc(scf.NETWORTH, scf.WGT, mean(scf.NETWORTH, scf.WGT))
-sw[4]=quant(scf.NETWORTH, scf.WGT, .99)/quant(scf.NETWORTH, scf.WGT, .5)
-sw[5]=quant(scf.NETWORTH, scf.WGT, .9)/quant(scf.NETWORTH, scf.WGT, .5)
-sw[6]=mean(scf.NETWORTH, scf.WGT)/quant(scf.NETWORTH, scf.WGT, .5)
-sw[7]=quant(scf.NETWORTH, scf.WGT, .5)/quant(scf.NETWORTH, scf.WGT, .3)
+sw[3]=stats.loc(scf.NETWORTH, scf.WGT, stats.mean(scf.NETWORTH, scf.WGT))
+sw[4]=stats.quantile_1D(scf.NETWORTH, scf.WGT, .99)/stats.quantile_1D(scf.NETWORTH, scf.WGT, .5)
+sw[5]=stats.quantile_1D(scf.NETWORTH, scf.WGT, .9)/stats.quantile_1D(scf.NETWORTH, scf.WGT, .5)
+sw[6]=stats.mean(scf.NETWORTH, scf.WGT)/stats.quantile_1D(scf.NETWORTH, scf.WGT, .5)
+sw[7]=stats.quantile_1D(scf.NETWORTH, scf.WGT, .5)/stats.quantile_1D(scf.NETWORTH, scf.WGT, .3)
 
 data_for_plotting=pd.DataFrame(np.vstack((si,sw)),columns=['Coefficient of variation','Variance of logs',\
      'Gini indexes','Location of mean','99-50 ratio', '90-50 ratio', 'Mean-to-median ratio',\
@@ -50,3 +55,7 @@ data_for_plotting=data_for_plotting.T
 
 print('\n'+'TABLE 3')
 print(data_for_plotting)
+
+
+
+
--- data/stats.py
+++ data/stats.py
@@ -6,6 +6,9 @@ Calculates the statistics using the weights.
 
 @author: Mohammad
 """
+
+import numpy as np
+
 def mean(series, weights):
     return (series*weights).sum()/weights.sum()
 
@@ -13,7 +16,7 @@ def variance(series, weights):
     var = mean((series-mean(series, weights))**2, weights)
     return var
     
-def variation(series, weights): 
+def coeff_variation(series, weights): 
     return variance(series, weights)**.5 / mean(series, weights)
     
 def quant(series, weights, quantile):
@@ -55,3 +58,49 @@ def loc(series, weights, a):
         s += weights[sorted_series.index[i]]/2
         i +=1
     return s/weights.sum()
+    
+    
+    
+def quantile_1D(data, weights, quantile):
+    """
+    Compute the weighted quantile of a 1D numpy array.
+    Parameters
+    ----------
+    data : ndarray
+        Input array (one dimension).
+    weights : ndarray
+        Array with the weights of the same size of `data`.
+    quantile : float
+        Quantile to compute. It must have a value between 0 and 1.
+    Returns
+    -------
+    quantile_1D : float
+        The output value.
+    """
+    # Check the data
+    if not isinstance(data, np.matrix):
+        data = np.asarray(data)
+    if not isinstance(weights, np.matrix):
+        weights = np.asarray(weights)
+    nd = data.ndim
+    if nd != 1:
+        raise TypeError("data must be a one dimensional array")
+    ndw = weights.ndim
+    if ndw != 1:
+        raise TypeError("weights must be a one dimensional array")
+    if data.shape != weights.shape:
+        raise TypeError("the length of data and weights must be the same")
+    if ((quantile > 1.) or (quantile < 0.)):
+        raise ValueError("quantile must have a value between 0. and 1.")
+    # Sort the data
+    ind_sorted = np.argsort(data)
+    sorted_data = data[ind_sorted]
+    sorted_weights = weights[ind_sorted]
+    # Compute the auxiliary arrays
+    Sn = np.cumsum(sorted_weights)
+    # TODO: Check that the weights do not sum zero
+    #assert Sn != 0, "The sum of the weights must not be zero"
+    Pn = (Sn-0.5*sorted_weights)/np.sum(sorted_weights)
+    # Get the value of the weighted median
+    return np.interp(quantile, Pn, sorted_data)
+    
