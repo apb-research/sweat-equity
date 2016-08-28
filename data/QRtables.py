03fd13c022c87fbeecac41d796f4493c4cc68cbd
anmol <bhandari@umn.edu>
2016-08-28 15:51:01 -0500

    wage and income by business type

--- data/QRtables.py
+++ data/QRtables.py
@@ -11,8 +11,8 @@ import stats
 pd.options.display.float_format = '${:,.0f}'.format
 
 
-
 scf = pd.read_csv('scf.csv')
+
 
 ################## Table 2
 i =0
@@ -27,50 +27,83 @@ pd.options.display.float_format = '{:,.2f}'.format
 
 ################## Table 3
 #Income (INCOME2 can also be used!)
-si=stats.describe(scf.INCOME,scf.WGT)
-# Networth
-sw=stats.describe(scf.NETWORTH,scf.WGT)
 
-data_for_plotting=pd.DataFrame(np.vstack((si,sw)),columns=['Coefficient of variation','Variance of logs',\
-     'Gini indexes','Location of mean','99-50 ratio', '90-50 ratio', 'Mean-to-median ratio',\
-     '50-30 ratio'], index=['income', 'wealth'])
-data_for_plotting=data_for_plotting.T
+data_for_plotting=pd.concat([stats.describe(scf.NETWORTH,scf.WGT),stats.describe(scf.INCOME,scf.WGT)],axis=1)
 
+data_for_plotting.columns=['INCOME','NETWORTH']
 print('\n'+'TABLE 3')
 print(data_for_plotting)
 
 #
-#
-moments=['Coefficient of variation','Variance of logs',\
-     'Gini indexes','Location of mean','99-50 ratio', '90-50 ratio', 'Mean-to-median ratio',\
-     '50-30 ratio']
      
-income_by_busorg=pd.DataFrame(np.zeros((len(moments), len(pd.unique(scf.BUSORG)))))
-income_by_busorg.index=moments
-
+income_by_busorg=[]
+colnames=[]
 business_types_codes={1:'P', 2:'SP', 3:'S',\
 4:'C', 6:'Foreign',11:'LLP',0:'No-Busines'}
-income_by_busorg.columns=business_types_codes.values()
 
 for bus in pd.unique(scf.BUSORG):
-    income_by_busorg[business_types_codes[bus]]=stats.describe(scf[scf.BUSORG==bus].INCOME,scf[scf.BUSORG==bus].WGT)
-#
-income_by_busorg['overall']=si
+    income_by_busorg.append(stats.describe(scf[scf.BUSORG==bus].INCOME,scf[scf.BUSORG==bus].WGT))
+    colnames.append(business_types_codes[bus])
 
 
-networth_by_busorg=pd.DataFrame(np.zeros((len(moments), len(pd.unique(scf.BUSORG)))))
-networth_by_busorg.index=moments
-networth_by_busorg.columns=business_types_codes.values()
-networth_by_busorg['overall']=sw
-
-for bus in pd.unique(scf.BUSORG):
-    networth_by_busorg[business_types_codes[bus]]=stats.describe(scf[scf.BUSORG==bus].NETWORTH,scf[scf.BUSORG==bus].WGT)
-
-
+income_by_busorg= pd.concat(income_by_busorg,axis=1)
+income_by_busorg.columns=colnames
+income_by_busorg['overall']=data_for_plotting['INCOME']
 print('\n'+'TABLE 4: INCOME by BUSORG')
 print(income_by_busorg)
 
 
+
+networth_by_busorg=[]
+colnames=[]
+business_types_codes={1:'P', 2:'SP', 3:'S',\
+4:'C', 6:'Foreign',11:'LLP',0:'No-Busines'}
+
+for bus in pd.unique(scf.BUSORG):
+    networth_by_busorg.append(stats.describe(scf[scf.BUSORG==bus].NETWORTH,scf[scf.BUSORG==bus].WGT))
+    colnames.append(business_types_codes[bus])
+
+
+networth_by_busorg= pd.concat(networth_by_busorg,axis=1)
+networth_by_busorg.columns=colnames
+networth_by_busorg['overall']=data_for_plotting['NETWORTH']
 print('\n'+'TABLE 5: NETWORTH by BUSORG')
 print(networth_by_busorg)
 
+scorpwages=scf[(scf.BUSORG==3)&(scf.WAGEINC>0)].WAGEINC
+scorpwages_wgts=scf[(scf.BUSORG==3)&(scf.WAGEINC>0)].WGT
+
+scorpincome=scf[(scf.BUSORG==3)&(scf.WAGEINC>0)].INCOME
+scorpincome_wgts=scf[(scf.BUSORG==3)&(scf.WAGEINC>0)].WGT
+
+scorps=pd.concat([stats.describe(scorpwages,scorpwages_wgts),stats.describe(scorpincome,scorpincome_wgts)],axis=1)
+scorps.columns=['S-WAGE','S-INCOME']
+
+
+
+partnershipwages=scf[(scf.BUSORG==1)&(scf.WAGEINC>0)].WAGEINC
+partnershipwages_wgts=scf[(scf.BUSORG==1)&(scf.WAGEINC>0)].WGT
+
+partnershipincome=scf[(scf.BUSORG==1)&(scf.WAGEINC>0)].INCOME
+partnershipincome_wgts=scf[(scf.BUSORG==1)&(scf.WAGEINC>0)].WGT
+
+partnerships=pd.concat([stats.describe(partnershipwages,partnershipwages_wgts),stats.describe(partnershipincome,partnershipincome_wgts)],axis=1)
+partnerships.columns=['P-WAGE','P-INCOME']
+
+
+
+
+solepropwages=scf[(scf.BUSORG==2)&(scf.WAGEINC>0)].WAGEINC
+solepropwages_wgts=scf[(scf.BUSORG==2)&(scf.WAGEINC>0)].WGT
+
+solepropincome=scf[(scf.BUSORG==2)&(scf.WAGEINC>0)].INCOME
+solepropincome_wgts=scf[(scf.BUSORG==2)&(scf.WAGEINC>0)].WGT
+
+soleprops=pd.concat([stats.describe(solepropwages,solepropwages_wgts),stats.describe(solepropincome,solepropincome_wgts)],axis=1)
+soleprops.columns=['SP-WAGE','SP-INCOME']
+
+
+
+wage_income=pd.concat([soleprops,partnerships,scorps],axis=1)
+print('\n'+'TABLE 5: INCOME,WAGES by BUSORG')
+print wage_income
